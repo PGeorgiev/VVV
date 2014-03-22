4ee1604165d343678b2d1835138b036ce58922f2
Ian Dunn <ian@iandunn.name>
2014-03-21 18:38:50 -0700

    Include full path in $CURRENT_SITE

--- www/vvv-init.sh
+++ www/vvv-init.sh
@@ -1,37 +1,38 @@
 echo ""
 
-CURRENT_SITE="misc.dev"
+CURRENT_SITE="/srv/www/misc.dev"
 echo "Provisioning $CURRENT_SITE"
-if [[ ! -d /srv/www/$CURRENT_SITE/wptest ]]; then
-	git clone --recursive https://github.com/manovotny/wptest.git /srv/www/$CURRENT_SITE/wptest
+if [[ ! -d $CURRENT_SITE/wptest ]]; then
+	git clone --recursive https://github.com/manovotny/wptest.git $CURRENT_SITE/wptest
 fi
 
 
-CURRENT_SITE="supportpress.dev"
+CURRENT_SITE="/srv/www/supportpress.dev"
 echo "Provisioning $CURRENT_SITE"
-if [ ! -d /srv/www/$CURRENT_SITE ]; then
-	svn co https://supportpress.svn.wordpress.org/trunk /srv/www/$CURRENT_SITE
-	cp /srv/www/$CURRENT_SITE/config-sample.php /srv/www/$CURRENT_SITE/config.php
+if [ ! -d $CURRENT_SITE ]; then
+	svn co https://supportpress.svn.wordpress.org/trunk $CURRENT_SITE
+	cp $CURRENT_SITE/config-sample.php $CURRENT_SITE/config.php
 
-	sed -i 's/putyourdbnamehere/supportpress_dev/'  /srv/www/$CURRENT_SITE/config.php
-	sed -i 's/usernamehere/macenzie/'               /srv/www/$CURRENT_SITE/config.php
-	sed -i 's/yourpasswordhere/password/'           /srv/www/$CURRENT_SITE/config.php
+	sed -i 's/putyourdbnamehere/supportpress_dev/'  $CURRENT_SITE/config.php
+	sed -i 's/usernamehere/macenzie/'               $CURRENT_SITE/config.php
+	sed -i 's/yourpasswordhere/password/'           $CURRENT_SITE/config.php
 
 	# todo sed other config values
+	# error_reporting( E_ALL ^ E_STRICT );
 fi
 
 
-CURRENT_SITE="wordcamp.dev"
+CURRENT_SITE="/srv/www/wordcamp.dev"
 echo "Provisioning $CURRENT_SITE"
-if [ ! -d /srv/www/$CURRENT_SITE ]; then
-	wp core download --path=/srv/www/$CURRENT_SITE/wordpress
-	svn co https://wordcamp.svn.wordpress.org/bin        /srv/www/$CURRENT_SITE/bin        --username=iandunn
-	svn co https://wordcamp.svn.wordpress.org/wp-content /srv/www/$CURRENT_SITE/wp-content --username=iandunn
+if [ ! -d $CURRENT_SITE ]; then
+	wp core download --path=$CURRENT_SITE/wordpress
+	svn co https://wordcamp.svn.wordpress.org/bin        $CURRENT_SITE/bin        --username=iandunn
+	svn co https://wordcamp.svn.wordpress.org/wp-content $CURRENT_SITE/wp-content --username=iandunn
 
-	cp /srv/www/$CURRENT_SITE/wp-config-sample.php    /srv/www/$CURRENT_SITE/wp-config.php
-	sed -i 's/database_name_here/wordcamp_dev/'       /srv/www/$CURRENT_SITE/wp-config.php
-	sed -i 's/username_here/macenzie/'                /srv/www/$CURRENT_SITE/wp-config.php
-	sed -i 's/password_here/password/'                /srv/www/$CURRENT_SITE/wp-config.php
+	cp $CURRENT_SITE/wp-config-sample.php                $CURRENT_SITE/wp-config.php
+	sed -i 's/database_name_here/wordcamp_dev/'          $CURRENT_SITE/wp-config.php
+	sed -i 's/username_here/macenzie/'                   $CURRENT_SITE/wp-config.php
+	sed -i 's/password_here/password/'                   $CURRENT_SITE/wp-config.php
 
 	# todo misc bits from wp-config like content_dir/url, multisite
 	# todo sandbox-functionality.php -- save to version control somewhere or something?
@@ -58,18 +59,19 @@ if [[ ! -d $CURRENT_SITE ]]; then
 	# todo svn/git co your plugins from wporg repo?
 fi
 
+
 # todo wp-develop.dev
 
 
-CURRENT_SITE="wp-skeleton.dev"
+CURRENT_SITE="/srv/www/wp-skeleton.dev"
 echo "Provisioning $CURRENT_SITE"
-if [[ ! -d /srv/www/$CURRENT_SITE ]]; then
-	git clone --recursive https://github.com/iandunn/WordPress-Skeleton.git /srv/www/$CURRENT_SITE
+if [[ ! -d $CURRENT_SITE ]]; then
+	git clone --recursive https://github.com/iandunn/WordPress-Skeleton.git $CURRENT_SITE
 
-	cp /srv/www/$CURRENT_SITE/environment-config-sample.php   /srv/www/$CURRENT_SITE/environment-config.php
-	sed -i "s/database_name_here/wp_skeleton_dev/"            /srv/www/$CURRENT_SITE/environment-config.php
-	sed -i "s/username_here/macenzie/"                        /srv/www/$CURRENT_SITE/environment-config.php
-	sed -i 's/password_here/password/'                        /srv/www/$CURRENT_SITE/environment-config.php
+	cp $CURRENT_SITE/environment-config-sample.php   $CURRENT_SITE/environment-config.php
+	sed -i "s/database_name_here/wp_skeleton_dev/"   $CURRENT_SITE/environment-config.php
+	sed -i "s/username_here/macenzie/"               $CURRENT_SITE/environment-config.php
+	sed -i 's/password_here/password/'               $CURRENT_SITE/environment-config.php
 fi
 
 
