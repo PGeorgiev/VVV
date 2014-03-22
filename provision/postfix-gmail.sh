70f658084a187cc605cbee4184531c4cebe1dfab
Ian Dunn <ian@iandunn.name>
2014-03-22 16:02:22 -0700

    Accept username and password from user input

--- provision/postfix-gmail.sh
+++ provision/postfix-gmail.sh
@@ -7,13 +7,18 @@ smtp_sasl_security_options = noanonymous
 smtp_tls_CAfile = /etc/postfix/cacert.pem
 smtp_use_tls = yes"
 
+echo -n "Enter full username (e.g., foo@gmail.com): "
+read USERNAME
+echo -n "Enter password: "
+read PASSWORD
+
 sudo sed -i '/relayhost/d' /etc/postfix/main.cf
 sudo echo -e "$RELAYHOST" >> /etc/postfix/main.cf
 
-sudo echo "[smtp.gmail.com]:587 username@gmail.com:password" >> /etc/postfix/sasl_passwd_tmp
+sudo echo "[smtp.gmail.com]:587 $USERNAME:$PASSWORD" >> /etc/postfix/sasl_passwd_tmp
 sudo chmod 400 /etc/postfix/sasl_passwd
 sudo postmap /etc/postfix/sasl_passwd
 cat /etc/ssl/certs/Thawte_Premium_Server_CA.pem | sudo tee -a /etc/postfix/cacert.pem_tmp
 sudo /etc/init.d/postfix reload
 
-echo "Postfix configuration done. You still need to update /etc/postfix/sasl_passwd with your username/password."
+echo "Postfix configuration done."
