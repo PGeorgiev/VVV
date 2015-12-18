2c65e6f204e712fd27935ac40121ceb308b9df7e
Ian Dunn <ian@iandunn.name>
2015-12-18 15:29:28 -0800

    Track provision-custom and provision-post

---
+++ provision/provision-post.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# TODO: You need to manually add `source` commands to the default .bashrc file in order to load .bashrc, .bash_aliases and .bash_prompt
+# That's the best way, so that you don't have to modify any of VVV's tracked files
+
+HOME_DIR="/home/vagrant"
+DOTFILES_DIR="$HOME_DIR/dotfiles"
+
+if [ ! -d $DOTFILES_DIR ]; then
+	git clone https://github.com/iandunn/dotfiles.git $DOTFILES_DIR
+else
+	git -C $DOTFILES_DIR pull origin master
+fi
+
+ln -sf "$DOTFILES_DIR/.gitconfig"        "$HOME_DIR/.gitconfig"
+ln -sf "$DOTFILES_DIR/.gitignore_global" "$HOME_DIR/.gitignore_global"
+
+sudo chown -R vagrant:vagrant "$HOME_DIR/.subversion"
+cp "$DOTFILES_DIR/.subversion/config" "$HOME_DIR/.subversion/config"
+cp "$DOTFILES_DIR/.ssh/config"        "$HOME_DIR/.ssh/config"
+
+# todo what to do about .subversion/servers ? append to existing?
